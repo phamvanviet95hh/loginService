e1dea2a570c4f75ac2514b343d20c09500882435
ASUS1 <vietpv@gtel.vn>
2024-05-30 08:15:41 +0700

    Initial commit

---
+++ src/main/java/com/gtelservice/loginservice/models/StarffModel.java
@@ -0,0 +1,12 @@
+package com.gtelservice.loginservice.models;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class StarffModel {
+
+    @Id
+    private String id;
+
+}
